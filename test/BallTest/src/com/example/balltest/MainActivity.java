d4b3c1ac21748863e009588d146c31f1b6034284
Pascal Zingg <pazingg@gmail.com>
2013-03-23 13:03:11 +0100

    Test

--- test/BallTest/src/com/example/balltest/MainActivity.java
+++ test/BallTest/src/com/example/balltest/MainActivity.java
@@ -1,40 +1,40 @@
 package com.example.balltest;
 
-import java.util.List;
-
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.os.Bundle;
-import android.app.Activity;
 import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements SensorEventListener {
 	private SensorManager sensorManager;
 	Bitmap ball;
 	float x, y, sensorX, sensorY;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		//Full-Screen
+		// Full-Screen
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-	            WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	    
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		if (sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) {
-			Sensor s = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
-			sensorManager.registerListener(this, s, SensorManager.SENSOR_DELAY_GAME);
+			Sensor s = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER)
+					.get(0);
+			sensorManager.registerListener(this, s,
+					SensorManager.SENSOR_DELAY_GAME);
 		}
 		ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
 		x = y = sensorX = sensorY = 0;
@@ -50,25 +50,20 @@ public class MainActivity extends Activity implements SensorEventListener {
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		sensorX = event.values[0];
 		sensorY = event.values[1];
-		TextView textX = (TextView)findViewById(R.id.textX);
-		TextView textY = (TextView)findViewById(R.id.textY);
+		TextView textX = (TextView) findViewById(R.id.textX);
+		TextView textY = (TextView) findViewById(R.id.textY);
 		textX.setText("x:" + sensorX);
 		textY.setText("y:" + sensorY);
-		ImageView ball = (ImageView)findViewById(R.id.ball);
+		ImageView ball = (ImageView) findViewById(R.id.ball);
 		ball.setX(ball.getX() - (sensorX * 2f));
 		ball.setY(ball.getY() + (sensorY * 2f));
-	}
-	
-	private void setBallPosition(float x, float y) {
-		//textX.setText(Float.toString(x));
-		//textY.setText(Float.toString(y));
 	}
 
 }
