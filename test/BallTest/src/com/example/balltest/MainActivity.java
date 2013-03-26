7dd8dbc85aa330938a021aa0f8c72c48ba674483
YanuFreaky <yanufreaky@gmail.com>
2013-03-26 15:00:21 +0100

    Some Balltests..

--- test/BallTest/src/com/example/balltest/MainActivity.java
+++ test/BallTest/src/com/example/balltest/MainActivity.java
@@ -1,43 +1,22 @@
 package com.example.balltest;
 
-import java.util.Random;
-
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
 	private SensorManager sensorManager;
-	Bitmap ball;
-	float x, y, sensorX, sensorY;
-
-	// Generates a Colorflash (Caution: Eyecancer)
-	class RenderView extends View {
-		Random rand = new Random();
-
-		public RenderView(Context context) {
-			super(context);
-		}
-
-		protected void onDraw(Canvas canvas) {
-			canvas.drawRGB(rand.nextInt(256), rand.nextInt(256),
-					rand.nextInt(256));
-			invalidate();
-		}
-	}
+	// Bitmap ball;
+	Ball ball;
+	float x, y, accelX, accelY;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,26 +26,25 @@ public class MainActivity extends Activity implements SensorEventListener {
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		super.onCreate(savedInstanceState);
-		setContentView(new RenderView(this));
+		// Creates a new RenderView (Ball)
+
+		// Something don't work here > Could not read file.
+		// InputStream ims = getAssets().open("ball.png");
+		// Picture picture = Picture.createFromStream(ims);
+		ball = new Ball(this);
+		setContentView(ball);
 		// setContentView(R.layout.activity_main);
-
-		// sensorManager = (SensorManager)
-		// getSystemService(Context.SENSOR_SERVICE);
-		// if (sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() ==
-		// 0) {
-		// Log.d("Message", "No accelerometer installed");
-		// } else {
-		// Sensor accelerometer = sensorManager.getSensorList(
-		// Sensor.TYPE_ACCELEROMETER).get(0);
-		// if (!sensorManager.registerListener(this, accelerometer,
-		// SensorManager.SENSOR_DELAY_GAME)) {
-		// Log.d("Message", "Couldn't register sensor listener");
-		// }
-		// }
-
-		ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
-		x = y = sensorX = sensorY = 0;
-
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		if (sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() == 0) {
+			Log.d("Message", "No accelerometer installed");
+		} else {
+			Sensor accelerometer = sensorManager.getSensorList(
+					Sensor.TYPE_ACCELEROMETER).get(0);
+			if (!sensorManager.registerListener(this, accelerometer,
+					SensorManager.SENSOR_DELAY_FASTEST)) {
+				Log.d("Message", "Couldn't register sensor listener");
+			}
+		}
 	}
 
 	@Override
@@ -84,15 +62,19 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		sensorX = event.values[0];
-		sensorY = event.values[1];
-		TextView textX = (TextView) findViewById(R.id.textX);
-		TextView textY = (TextView) findViewById(R.id.textY);
-		textX.setText("x:" + sensorX);
-		textY.setText("y:" + sensorY);
-		ImageView ball = (ImageView) findViewById(R.id.ball);
-		ball.setX(ball.getX() - (sensorX * 2f));
-		ball.setY(ball.getY() + (sensorY * 2f));
+		accelX = event.values[0];
+		accelY = event.values[1];
+		updateBall(accelX, accelY);
 	}
 
+	private void updateBall(float accelX, float accelY) {
+		int displayWidth = ball.getWidth();
+		int displayHeight = ball.getHeight();
+		// if ((ball.xPosition + (accelX * 2f)) > displayWidth) {
+		// } else {
+		ball.playGround.contains((int)(ball.xPosition - (accelX * 2f)), (int)(ball.yPosition + (accelY * 2f)))
+		ball.xPosition = ball.xPosition - (accelX * 2f);
+		// }
+		ball.yPosition = ball.yPosition + (accelY * 2f);
+	}
 }
