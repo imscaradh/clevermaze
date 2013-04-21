454e2f0654f89ea6b23ff7d3a0ff2d1934b4db2c
Pascal Zingg <pazingg@gmail.com>
2013-04-21 19:52:21 +0200

    clean code

--- CleverMaze/src/ch/gibb/project/activity/Activity.java
+++ CleverMaze/src/ch/gibb/project/activity/Activity.java
@@ -2,12 +2,14 @@ package ch.gibb.project.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public abstract class Activity extends android.app.Activity {
 
 	protected void onCreate(Bundle savedInstanceState, int activity) {
 		super.onCreate(savedInstanceState);
 		setContentView(activity);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		initObjects();
 	}
 
