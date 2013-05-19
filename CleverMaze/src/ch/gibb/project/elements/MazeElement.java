d901acbf534b8839c0aa036d7dbb2950c796d427
YanuFreaky <yanufreaky@gmail.com>
2013-05-19 16:32:16 +0200

    Removed some useless methods

--- CleverMaze/src/ch/gibb/project/elements/MazeElement.java
+++ CleverMaze/src/ch/gibb/project/elements/MazeElement.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Point;
-import android.os.Debug;
 import android.util.DisplayMetrics;
 import android.view.View;
 import ch.gibb.project.activity.Level;
@@ -32,32 +31,6 @@ public class MazeElement extends View {
 
 	public void setPosition(Point position) {
 		this.position = position;
-	}
-
-	/**
-	 * Checks if a bitmap with the specified size fits in memory
-	 * 
-	 * @param bmpwidth
-	 *            Bitmap width
-	 * @param bmpheight
-	 *            Bitmap height
-	 * @param bmpdensity
-	 *            Bitmap bpp (use 2 as default)
-	 * @return true if the bitmap fits in memory false otherwise
-	 */
-	public static boolean checkBitmapFitsInMemory(long bmpwidth,
-			long bmpheight, int bmpdensity) {
-		long reqsize = bmpwidth * bmpheight * bmpdensity;
-		long allocNativeHeap = Debug.getNativeHeapAllocatedSize();
-
-		final long heapPad = (long) Math.max(4 * 1024 * 1024, Runtime
-				.getRuntime().maxMemory() * 0.1);
-		if ((reqsize + allocNativeHeap + heapPad) >= Runtime.getRuntime()
-				.maxMemory()) {
-			return false;
-		}
-		return true;
-
 	}
 
 }
